4ee335f4b37281424899ef857c37d2190cdaa828
Fherna Leian Caoili <fherna.caoili@gmail.com>
2017-11-29 16:00:27 -0800

    Add Files

---
+++ ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Studio Ghibli"),
+  
+  # Sidebar
+  sidebarLayout(
+    
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       
+    )
+  )
+))
